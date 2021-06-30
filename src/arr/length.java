b2f76727fd4179e8fbb53bec112c9413d2dbe856
SBhagya97 <bhagyascbc1997@gmail.com>
2021-07-01 01:58:20 +0530

    Bubble sort

---
+++ src/arr/length.java
@@ -0,0 +1,4 @@
+package arr;
+
+public enum length {
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,31 @@
+package com.company;
+
+public class Main {
+
+
+
+    public static void main(String[] args) {
+	// write your code here
+        int arr[]={1,2,3,4,5};
+
+
+        for(int i = 0;i< arr.length-1;i++){//outer loop for passes
+            int flag=0;
+            for(int j=0;j<arr.length-1-i;j++){//inner loop  for sort
+                if(arr[j]<arr[j+1]){
+                    int temp;
+                    temp=arr[j+1];
+                    arr[j+1]=arr[j];
+                    arr[j]=temp;
+                    flag=flag+1;
+                }
+            }
+            if(flag==0){
+               break;
+            }
+        }
+        for(int i=0;i<arr.length;i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
